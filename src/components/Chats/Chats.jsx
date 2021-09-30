b23d9e1dbeae2d890f7e2c400edbdafcc6b6228b
Dmitry <dim.smir@inbox.ru>
2021-09-30 16:57:52 +0600

    home work 9

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useMemo, useState, useEffect, useRef} from 'react';
 import {useParams, useHistory} from 'react-router-dom';
 import {Button} from 'reactstrap';
 import {Link} from 'react-router-dom';
@@ -8,39 +8,62 @@ import './Chats.scss';
 import FormMess from '../Form/FormMess.jsx';
 import SideBar from '../SideBar/SideBar';
 import Message from '../Messages/Message';
-import {addChat, deleteChatItem} from '../../store/chats/actions';
-import {addMessageWithReply} from '../../store/messages/actions';
+// import {addChat, deleteChatItem} from '../../store/chats/actions';
+// import {addMessageWithReply} from '../../store/messages/actions';
+import {db} from '../../Services/firebase';
+import {ref, set, onValue} from 'firebase/database';
 
 const Chats = () => {
   const {chatId} = useParams();
   const history = useHistory();
-  const dispatch = useDispatch();
 
-  const chats = useSelector((state) => state.chats.chats);
-  const messageList = useSelector((state) => state.messages.messages);
+  //чаты из бд
+  const [chats, setChats] = useState([]);
+  useEffect(() => {
+    //userDbRef получаем базу данных, второй аргумент - раздел бд
+    const chatsDbRef = ref(db, 'chats');
+    // получаем снимок бд и приобразуем в объект методом val()
+    onValue(chatsDbRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log('--------', data);
+      setChats(Object.values(data || {}));
+    });
+  }, []);
+
+  const handleAddChat = (name) => {
+    const newId = `chat-${Date.now()}`;
+    const chatsDbRef = ref(db, `chats/${newId}`);
+    set(chatsDbRef, {id: newId, name: name});
+  };
+
+  // добавление сообщений через БД
+  const unsubscribeMessages = useRef(null);
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    if (unsubscribeMessages.current) {
+      unsubscribeMessages.current();
+    }
+    const messagesDbRef = ref(db, `messages/${chatId}`);
+    const unsubscribe = onValue(messagesDbRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log('--------', data);
+      setMessages(Object.values(data || {}));
+    });
+    unsubscribeMessages.current = unsubscribe;
+    return () => unsubscribe();
+  }, [chatId]);
 
   const sendMessage = useCallback(
     ({text, author}) => {
-      dispatch(addMessageWithReply(chatId, text, author));
+      const newId = `message-${Date.now()}`;
+      const messagesDbRef = ref(db, `messages/${chatId}/${newId}`);
+      set(messagesDbRef, {
+        author,
+        text,
+        id: newId,
+      });
     },
-    [chatId, dispatch],
-  );
-
-  const AddChat = useCallback(
-    (name) => {
-      dispatch(addChat(name));
-    },
-    [dispatch],
-  );
-
-  const deleteChat = useCallback(
-    (id) => {
-      dispatch(deleteChatItem(id));
-      if (chatId === id) {
-        history.push('/chats');
-      }
-    },
-    [chatId, history, dispatch],
+    [chatId],
   );
 
   const subForm = useCallback(
@@ -49,6 +72,42 @@ const Chats = () => {
     },
     [sendMessage],
   );
+
+  // const messageList = useSelector((state) => state.messages.messages);
+  // const dispatch = useDispatch();
+  // чаты и сообщения из редакса
+
+  // const chats = useSelector((state) => state.chats.chats);
+  // const sendMessage = useCallback(
+  //   ({text, author}) => {
+  //     dispatch(addMessageWithReply(chatId, text, author));
+  //   },
+  //   [chatId, dispatch],
+  // );
+
+  // const AddChat = useCallback(
+  //   (name) => {
+  //     dispatch(addChat(name));
+  //   },
+  //   [dispatch],
+  // );
+
+  // const deleteChat = useCallback(
+  //   (id) => {
+  //     dispatch(deleteChatItem(id));
+  //     if (chatId === id) {
+  //       history.push('/chats');
+  //     }
+  //   },
+  //   [chatId, history, dispatch],
+  // );
+
+  // const subForm = useCallback(
+  //   (message) => {
+  //     sendMessage(message);
+  //   },
+  //   [sendMessage],
+  // );
 
   const chatExist = useMemo(() => !!chats.find(({id}) => id === chatId), [chatId, chats]);
 
@@ -61,13 +120,13 @@ const Chats = () => {
         </Button>{' '}
       </Link>
       <div className='main-display'>
-        <SideBar chats={chats} addChat={AddChat} deleteChat={deleteChat} />
+        <SideBar chats={chats} addChat={handleAddChat} />
         <div>
           {chatId && chatExist ? (
             <>
               <FormMess subForm={subForm} />
               <ListGroup className='list'>
-                <Message messageList={messageList} chatId={chatId}></Message>
+                <Message messages={messages} chatId={chatId}></Message>
               </ListGroup>
             </>
           ) : chats.length === 0 ? (
